c164129c4f91131eba5fd2eb01195d2cfbc2e851
danish <danishmehmood981@gmail.com>
2024-07-10 12:06:12 +0500

    minor change

--- src/components/EnergyCommunityTable.vue
+++ src/components/EnergyCommunityTable.vue
@@ -1,53 +1,8 @@
 <template>
 
   <!-- TODO: Implement data table with CRUD operations -->
-
-
+   
   <div class="middle-centered">
-    <!--    <v-data-table-->
-    <!--        density="compact"-->
-    <!--        items-per-page="5">-->
-    <!--      <thead class="border border-b">-->
-    <!--      <tr class="bg-grey-lighten-3">-->
-    <!--        <th class="text-left font-weight-bold">-->
-    <!--          Id-->
-    <!--        </th>-->
-    <!--        <th class="text-left font-weight-bold">-->
-    <!--          Name-->
-    <!--        </th>-->
-    <!--        <th class="text-left font-weight-bold">-->
-    <!--          Usage-->
-    <!--        </th>-->
-    <!--        <th class="text-left font-weight-bold">-->
-    <!--          Members-->
-    <!--        </th>-->
-    <!--        <th class="text-left font-weight-bold">-->
-    <!--          Location-->
-    <!--        </th>-->
-    <!--        <th class="text-left font-weight-bold">-->
-    <!--          Type-->
-    <!--        </th>-->
-    <!--        <th class="text-left font-weight-bold">-->
-    <!--          Date-->
-    <!--        </th>-->
-    <!--      </tr>-->
-    <!--      </thead>-->
-    <!--      <tbody>-->
-    <!--      <tr-->
-    <!--          v-for="item in energyCommunities"-->
-    <!--          :key="item.id"-->
-    <!--      >-->
-    <!--        <td class="text-left font-weight-bold">{{ item.id }}</td>-->
-    <!--        <td class="text-left">{{ item.name }}</td>-->
-    <!--        <td class="text-left">{{ item.energyUsage }}</td>-->
-    <!--        <td class="text-left">{{ item.members }}</td>-->
-    <!--        <td class="text-left">{{ item.location }}</td>-->
-    <!--        <td class="text-left">{{ item.energyType }}</td>-->
-    <!--        <td class="text-left">{{ item.startDate }}</td>-->
-    <!--      </tr>-->
-    <!--      </tbody>-->
-    <!--    </v-data-table>-->
-
     <v-data-table
         density="compact"
         :headers="[
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,18 @@
 <script setup>
-import AutocompleteSelect from '@/components/AutocompleteSelect.vue';
-import Charts from '@/components/Charts.vue';
+import AutocompleteSelect from "@/components/AutocompleteSelect.vue";
+import Charts from "@/components/Charts.vue";
 import EnergyCommunityTable from "@/components/EnergyCommunityTable.vue";
 </script>
 
 <template>
   <div>
     <!-- Dev server is running: TODO go on implementing the frontend -->
-
-    <h2 class="text-center ma-3 border-b">Energy Communities table | CRUD</h2>
-
+     
     <!-- todo : Place components here -->
     <autocomplete-select></autocomplete-select>
     <energy-community-table></energy-community-table>
 
-
     <!-- todo : chart should be in separate route / page -->
     <charts></charts>
-
   </div>
 </template>
