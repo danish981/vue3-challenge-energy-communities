abb1958ff6b3fc6e5ccbd20b0bbb103c087bd6df
danish <danishmehmood981@gmail.com>
2024-07-10 13:40:34 +0500

    MInor improvement in get communities

--- src/components/AutocompleteSelect.vue
+++ src/components/AutocompleteSelect.vue
@@ -4,7 +4,10 @@
   <div class="middle-centered">
     <v-autocomplete
         density="default"
+        v-model="selectedNames"
         label="Select energy communities by name"
+        item-title="name"
+        item-value="name"
         :items="names"
         chips
         bg-color="#E0E0E0"
@@ -20,7 +23,20 @@
 // TODO: Utilize Vue Composition API for managing states and logic
 
 import {energyCommunityStore} from "@/stores/energyCommunityStore";
-const names = (await energyCommunityStore().getCommunities()).map(community => community.name);
+import {ref, watch} from "vue";
+
+const store = energyCommunityStore();
+const names = ref([]);
+const selectedNames = ref([]);
+
+store.getCommunities().then(communities => {
+  names.value = communities.map(community => community.name);
+});
+
+watch(selectedNames, (newSelectedNames) => {
+  console.log(newSelectedNames)
+  store.selectedCommunities = newSelectedNames;
+});
 
 
 </script>
--- src/stores/energyCommunityStore.js
+++ src/stores/energyCommunityStore.js
@@ -6,17 +6,32 @@ export const energyCommunityStore = defineStore('energyCommunityStore', {
         // TODO: Define the state for storing energy communities data
         // Hint: You might need an array for communities and possibly other state variables for loading or error management
         communities: EnergyCommunityService.fetchCommunities(),
+        selectedCommunities: [],
         loading: false,
         error: null
     }),
+
+    getters: {
+        getSelectedCommunities: {
+            get: (state) => state.selectedCommunities
+        }
+    },
 
     actions: {
         // TODO: Implement an action to fetch communities data using the EnergyCommunityService
         // This should update the state appropriately with the data, loading status, and any errors
 
         async getCommunities() {
-            return EnergyCommunityService.fetchCommunities();
-        }
+            this.loading = true;
+            this.error = null;
+            try {
+                return this.communities = await EnergyCommunityService.fetchCommunities();
+            } catch (error) {
+                return this.error = error.message;
+            } finally {
+                this.loading = false;
+            }
+        },
 
         // TODO: Implement create, update, and delete actions for communities
         // Each action should interact with the EnergyCommunityService and update the state accordingly
