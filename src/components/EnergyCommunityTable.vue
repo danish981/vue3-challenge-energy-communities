96dd3fa22db6aa6611e2b92cbead3cf5528d6354
danish <danishmehmood981@gmail.com>
2024-07-18 15:56:50 +0500

    realtime search functionality added in the table

--- src/components/EnergyCommunityTable.vue
+++ src/components/EnergyCommunityTable.vue
@@ -1,50 +1,70 @@
 <template>
   <div class="middle-centered">
-    <v-data-table
-        density="compact"
-        :headers="headers"
-        class="elevation-1 text-left bg-amber-darken-2 bg-grey-lighten-5 border-md rounded"
-        :items="selectedCommunities ?? store.communities"
-        item-key="name"
-        items-per-page="10"
-    >
-      <template v-slot:top>
-        <v-btn color="primary" @click="openAddForm">Add Community</v-btn>
+
+
+    <v-card title="Energy Communities" flat>
+
+      <template v-slot:text>
+        <v-text-field
+            v-model="search"
+            label="Search"
+            prepend-inner-icon="mdi-magnify"
+            variant="outlined"
+            hide-details
+            single-line
+        ></v-text-field>
       </template>
 
 
-      <template v-slot:item.actions="{ item }">
-
-        <v-btn
-            variant="elevated"
-            @click="openEditForm(item)"
-            color="green"
-            text="edit"
-            class="mr-1 rounded"
-            size="small"
-            label
-        >
-          <v-icon color="white">mdi-pencil</v-icon>
-        </v-btn>
-
-
-        <v-btn
-            variant="elevated"
-            @click="deleteCommunity(item.id)"
-            color="red"
-            text="delete"
-            class="rounded"
-            size="small"
-            label
-        >
-          <v-icon color="white">mdi-bucket</v-icon>
-        </v-btn>
+      <v-data-table
+          density="compact"
+          :headers="headers"
+          class="elevation-1 text-left bg-amber-darken-2 bg-grey-lighten-5 border-md rounded"
+          :items="selectedCommunities ?? store.communities"
+          item-key="name"
+          :search="search"
+          items-per-page="10"
+      >
+        <template v-slot:top>
+          <v-btn color="primary" @click="openAddForm">Add Community</v-btn>
+        </template>
 
 
-      </template>
+        <template v-slot:item.actions="{ item }">
+
+          <v-btn
+              variant="elevated"
+              @click="openEditForm(item)"
+              color="green"
+              text="edit"
+              class="mr-1 rounded"
+              size="small"
+              label
+          >
+            <v-icon color="white">mdi-pencil</v-icon>
+          </v-btn>
 
 
-    </v-data-table>
+          <v-btn
+              variant="elevated"
+              @click="deleteCommunity(item.id)"
+              color="red"
+              text="delete"
+              class="rounded"
+              size="small"
+              label
+          >
+            <v-icon color="white">mdi-bucket</v-icon>
+          </v-btn>
+
+
+        </template>
+
+
+      </v-data-table>
+
+    </v-card>
+
 
     <v-dialog v-model="addoreditCommunityForm" max-width="600">
       <template v-slot:activator="{ on }"></template>
@@ -57,7 +77,8 @@
             </v-col>
 
             <v-col cols="12" md="4" sm="6">
-              <v-text-field label="Energy Usage*" placeholder="e,g 1500" required v-model="form.energyUsage"></v-text-field>
+              <v-text-field label="Energy Usage*" placeholder="e,g 1500" required
+                            v-model="form.energyUsage"></v-text-field>
             </v-col>
 
             <v-col cols="12" md="4" sm="6">
@@ -69,11 +90,13 @@
             </v-col>
 
             <v-col cols="12" md="4" sm="6">
-              <v-text-field label="Energy Type*" placeholder="e,g Solar" required v-model="form.energyType"></v-text-field>
+              <v-text-field label="Energy Type*" placeholder="e,g Solar" required
+                            v-model="form.energyType"></v-text-field>
             </v-col>
 
             <v-col cols="12" md="4" sm="6">
-              <v-text-field label="Start Date*" placeholder="e,g 2020-01-01" required v-model="form.startDate"></v-text-field>
+              <v-text-field label="Start Date*" placeholder="e,g 2020-01-01" required
+                            v-model="form.startDate"></v-text-field>
             </v-col>
           </v-row>
 
@@ -85,7 +108,8 @@
 
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="secondary rounded" variant="elevated" text="Close" @click="addoreditCommunityForm = false"></v-btn>
+          <v-btn color="secondary rounded" variant="elevated" text="Close"
+                 @click="addoreditCommunityForm = false"></v-btn>
           <v-btn color="primary rounded" variant="elevated" text="Save" @click="saveForm"></v-btn>
         </v-card-actions>
 
@@ -116,6 +140,8 @@ const headers = [
 
 const addoreditCommunityForm = ref(false);
 const isEditMode = ref(false);
+const search = ref('');
+
 const form = ref({
   id: null,
   name: '',
