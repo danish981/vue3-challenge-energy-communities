a9f8688c7b0f8be4d607c0a1d8054aa824aec8ec
danish <danishmehmood981@gmail.com>
2024-07-18 16:16:37 +0500

    app state pinia store rolled back into the code

---
+++ src/stores/appStateStore.js
@@ -0,0 +1,13 @@
+// src/store/appStateStore.js
+import { defineStore } from 'pinia';
+
+export const useAppStateStore = defineStore('appState', {
+    state: () => ({
+        // TODO: Define state variables to manage application-wide state
+    }),
+    actions: {
+        // TODO: Implement actions to set and clear loading state
+        // Example action: setLoading(loading: boolean)
+
+    }
+});
